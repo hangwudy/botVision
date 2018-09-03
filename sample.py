c0a2fde27841fd82ab401a784ee0f7025a8ad89d
hangwudy <hang.wu@yahoo.com>
2018-09-03 19:52:53 +0200

    test tensorflow

--- sample.py
+++ sample.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+import tensorflow as tf
+
+a = tf.constant("Hello World!")
+b = tf.constant(13)
+c = tf.constant(27)
+d = tf.add(b,c)
+
+with tf.Session() as sess:
+    print(sess.run(a).decode())
+    print(sess.run(d))
+
